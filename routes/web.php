5404096e0618c211ecf28fa0594cc7b9bbac4b47
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-05-20 18:49:07 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('path della rotta con eventuali parametri', ['nome completo di namespace della classe controller', 'metodo che gestisce la rotta'])->name('nome della rotta');
 
-// Route::get('/activities', [ActivitiesController::class, 'activities'])->name('activities');
+// rotte pagine generali
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+
+// rotte per la risorsa del sito
+Route::get('/books',            [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create',     [BookController::class, 'create'])->name('books.create');
+Route::get('/books/{id}',       [BookController::class, 'show'])->name('books.show');
+Route::get('/books/{id}/edit',  [BookController::class, 'edit'])->name('books.edit');
